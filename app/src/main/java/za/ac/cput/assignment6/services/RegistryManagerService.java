55b70f6f68cd99fec8eebda1cf5b31265c39bdf6
Ryan-Carstens <rcstringers@gmail.com>
2016-06-01 01:31:27 +0200

    select services working

---
+++ app/src/main/java/za/ac/cput/assignment6/services/RegistryManagerService.java
@@ -0,0 +1,15 @@
+package za.ac.cput.assignment6.services;
+
+import java.util.ArrayList;
+
+import za.ac.cput.assignment6.domain.RegistryManager;
+
+/**
+ * Created by sanXion on 2016/05/31.
+ */
+public interface RegistryManagerService {
+    RegistryManager storeRegistryManager(RegistryManager registryManager);
+    boolean updateRegistryManagerDetails(RegistryManager registryManager);
+    ArrayList<RegistryManager> registryManagerLogin(String email, String password);
+    ArrayList<RegistryManager> findByEmail(String email);
+}
